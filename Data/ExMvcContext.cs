76991148d6c19ef7da750440499fd153086afb83
Julien.Iz <69205008+IzyJu@users.noreply.github.com>
2020-11-25 14:48:22 +0100

    Add files via upload

---
+++ Data/ExMvcContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExMvc.Models;
+
+namespace ExMvc.Data
+{
+    public class ExMvcContext : DbContext
+    {
+        public ExMvcContext(DbContextOptions<ExMvcContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Articles> Articles { get; set; }
+    }
+}
---
+++ Models/Articles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExMvc.Models
+{
+    public class Articles
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Contenu { get; set; }
+    }
+}
